901937aba9208232b0e85584ca9d1ba3cdedb6ac
lazarodamasceno <Mylittleponey1@>
2024-10-02 23:31:35 -0300

    Created the util UserFinderUtil

---
+++ v1/src/main/java/com/api/v1/utils/UserFinderUtil.java
@@ -0,0 +1,24 @@
+package com.api.v1.utils;
+
+import com.api.v1.domain.User;
+import com.api.v1.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class UserFinderUtil {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Mono<User> find(String ssn) {
+        return userRepository
+                .findAll()
+                .filter(e -> e.getSsn().equals(ssn)
+                        && e.getDeactivatedAt() == null
+                )
+                .singleOrEmpty();
+    }
+
+}
