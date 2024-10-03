5025f802e3ded1995036baf12848e12543083a61
lazarodamasceno <Mylittleponey1@>
2024-10-03 04:02:06 -0300

    Fixed the message of DuplicatedSsnException and CustomerNotFoundException

--- v1/src/main/java/com/api/v1/exceptions/CustomerNotFoundException.java
+++ v1/src/main/java/com/api/v1/exceptions/CustomerNotFoundException.java
@@ -3,7 +3,7 @@ package com.api.v1.exceptions;
 public class CustomerNotFoundException extends RuntimeException {
 
     public CustomerNotFoundException(String ssn) {
-        super("Customer whose SSN is %s was not found.");
+        super("Customer whose SSN is %s was not found.".formatted(ssn));
     }
 
 }
--- v1/src/main/java/com/api/v1/exceptions/DuplicatedSsnException.java
+++ v1/src/main/java/com/api/v1/exceptions/DuplicatedSsnException.java
@@ -3,7 +3,7 @@ package com.api.v1.exceptions;
 public class DuplicatedSsnException extends RuntimeException {
 
     public DuplicatedSsnException(String ssn) {
-        super("The input SSN %s is already registered.");
+        super("The input SSN %s is already registered.".formatted(ssn));
     }
 
 }
