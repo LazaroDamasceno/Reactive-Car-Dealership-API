fd93e28001d74e010cf5bc45f56e8321821413ce
lazarodamasceno <Mylittleponey1@>
2024-10-03 00:42:24 -0300

    Created the entity Customer and its repository

---
+++ v1/src/main/java/com/api/v1/domain/Customer.java
@@ -0,0 +1,33 @@
+package com.api.v1.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.UUID;
+
+@Document(collection = "customers")
+@Getter
+@Builder
+public class Customer {
+
+    @Id
+    private UUID id;
+    private String address;
+    private User user;
+    private Instant createdAt;
+    private ZoneId createdAtZone;
+    private Instant modifiedAt;
+    private ZoneId modifiedAtZone;
+
+    public void modify(String address, User user) {
+        this.address = address;
+        this.user = user;
+        modifiedAt = Instant.now();
+        modifiedAtZone = ZoneId.systemDefault();
+    }
+
+}
