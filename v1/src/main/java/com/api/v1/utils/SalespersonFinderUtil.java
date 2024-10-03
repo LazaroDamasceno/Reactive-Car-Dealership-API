178fd2010b297c5d1b65983724a07e3d0f337be9
lazarodamasceno <Mylittleponey1@>
2024-10-03 20:46:50 -0300

    Created the util SalespersonFinderUtil

---
+++ v1/src/main/java/com/api/v1/utils/SalespersonFinderUtil.java
@@ -0,0 +1,26 @@
+package com.api.v1.utils;
+
+import com.api.v1.domain.salesperson.Salesperson;
+import com.api.v1.domain.salesperson.SalespersonRepository;
+import com.api.v1.exceptions.SalespersonNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.math.BigInteger;
+
+@Component
+public class SalespersonFinderUtil {
+
+    @Autowired
+    private SalespersonRepository salespersonRepository;
+
+    public Mono<Salesperson> find(String employeeId) {
+        return salespersonRepository
+                .findAll()
+                .filter(e -> e.getEmployeeId().equals(new BigInteger(employeeId)))
+                .singleOrEmpty()
+                .switchIfEmpty(Mono.error(new SalespersonNotFoundException(employeeId)));
+    }
+
+}
