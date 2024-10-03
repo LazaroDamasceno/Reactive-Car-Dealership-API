11727fe22519ff5ce5d790b83bda2219c3d35d6d
lazarodamasceno <Mylittleponey1@>
2024-10-03 19:35:10 -0300

    Renamed the SalespersonController's endpoint as "salespeople"

--- v1/src/main/java/com/api/v1/controllers/SalespersonController.java
+++ v1/src/main/java/com/api/v1/controllers/SalespersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("api/v1/salesperson")
+@RequestMapping("api/v1/salespeople")
 public class SalespersonController {
 
     @Autowired
