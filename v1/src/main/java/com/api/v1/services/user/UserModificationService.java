11b8b5ada615a4264494a92146ef3296bd491800
lazarodamasceno <Mylittleponey1@>
2024-10-03 13:31:33 -0300

    Moved UserModificationService to services/user

--- v1/src/main/java/com/api/v1/services/user/UserModificationService.java
+++ v1/src/main/java/com/api/v1/services/user/UserModificationService.java
@@ -1,6 +1,6 @@
-package com.api.v1.services;
+package com.api.v1.services.user;
 
-import com.api.v1.domain.User;
+import com.api.v1.domain.user.User;
 import com.api.v1.dtos.UserModificationRequestDto;
 import reactor.core.publisher.Mono;
 
