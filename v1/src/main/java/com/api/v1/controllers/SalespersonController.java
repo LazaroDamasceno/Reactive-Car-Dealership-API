aaca008c4f67fe04f22e2684fe0566bfcbcfad61
lazarodamasceno <Mylittleponey1@>
2024-10-03 21:40:49 -0300

    Created the service SalespersonModificationService

--- v1/src/main/java/com/api/v1/controllers/SalespersonController.java
+++ v1/src/main/java/com/api/v1/controllers/SalespersonController.java
@@ -1,9 +1,13 @@
 package com.api.v1.controllers;
 
 import com.api.v1.domain.salesperson.Salesperson;
+import com.api.v1.dtos.UserModificationRequestDto;
 import com.api.v1.dtos.UserRegistrationRequestDto;
+import com.api.v1.services.salesperson.SalespersonModificationService;
 import com.api.v1.services.salesperson.SalespersonRegistrationService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +20,21 @@ public class SalespersonController {
     @Autowired
     private SalespersonRegistrationService salespersonRegistrationService;
 
+    @Autowired
+    private SalespersonModificationService salespersonModificationService;
+
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public Mono<Salesperson> register(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return salespersonRegistrationService.register(requestDto);
     }
 
+    @PutMapping("{employeeId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<Salesperson> modify(
+            @NotBlank @Size(min=7, max=7) @PathVariable String employeeId,
+            @RequestBody UserModificationRequestDto requestDto
+    ) {
+        return salespersonModificationService.modify(employeeId, requestDto);
+    }
 }
---
+++ v1/src/main/java/com/api/v1/services/salesperson/SalespersonModificationService.java
@@ -0,0 +1,11 @@
+package com.api.v1.services.salesperson;
+
+import com.api.v1.domain.salesperson.Salesperson;
+import com.api.v1.dtos.UserModificationRequestDto;
+import reactor.core.publisher.Mono;
+
+public interface SalespersonModificationService {
+
+    Mono<Salesperson> modify(String employeeId, UserModificationRequestDto requestDto);
+
+}
