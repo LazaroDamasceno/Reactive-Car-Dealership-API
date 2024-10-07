31cbf185ed968dbd8e4d10283d53a2207ada54cc
lazarodamasceno <Mylittleponey1@>
2024-10-06 23:43:23 -0300

    Created the mapper PurchaseResponseMapper

---
+++ v1/src/main/java/com/api/v1/utils/purchases/PurchaseResponseMapper.java
@@ -0,0 +1,23 @@
+package com.api.v1.utils.purchases;
+
+import com.api.v1.domain.purchases.Purchases;
+import com.api.v1.dtos.purchases.PurchaseResponseDto;
+import com.api.v1.utils.cars.CarResponseMapper;
+import com.api.v1.utils.customers.CustomerResponseMapper;
+import com.api.v1.utils.salespeople.SalespersonResponseMapper;
+
+public class PurchaseResponseMapper {
+
+    public static PurchaseResponseDto map(Purchases purchase) {
+        return new PurchaseResponseDto(
+                purchase.idNumber(),
+                CarResponseMapper.map(purchase.car()),
+                CustomerResponseMapper.map(purchase.customers()),
+                SalespersonResponseMapper.map(purchase.salesperson()),
+                purchase.finalPrice(),
+                purchase.createdAt(),
+                purchase.createdAtZone()
+        );
+    }
+
+}
