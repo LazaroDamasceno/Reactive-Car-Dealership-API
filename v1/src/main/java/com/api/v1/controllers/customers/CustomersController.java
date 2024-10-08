ac7f23271a491686e5b93cf16692c352999f5ebd
lazarodamasceno <Mylittleponey1@>
2024-10-08 11:50:03 -0300

    Created the test class CustomerRetrievalBySsnTest and the service CustomerRetrievalService

--- v1/src/main/java/com/api/v1/controllers/customers/CustomersController.java
+++ v1/src/main/java/com/api/v1/controllers/customers/CustomersController.java
@@ -3,14 +3,17 @@ package com.api.v1.controllers.customers;
 import com.api.v1.domain.customers.Customers;
 import com.api.v1.dtos.customers.CustomerModificationRequestDto;
 import com.api.v1.dtos.customers.CustomerRegistrationRequestDto;
+import com.api.v1.dtos.customers.CustomerResponseDto;
 import com.api.v1.services.customers.CustomerModificationService;
 import com.api.v1.services.customers.CustomerRegistrationService;
+import com.api.v1.services.customers.CustomerRetrievalService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,6 +26,9 @@ public class CustomersController {
     @Autowired
     private CustomerModificationService customerModificationService;
 
+    @Autowired
+    private CustomerRetrievalService customerRetrievalService;
+
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public Mono<Customers> register(@Valid @RequestBody CustomerRegistrationRequestDto requestDto) {
@@ -30,11 +36,24 @@ public class CustomersController {
     }
 
     @PutMapping("{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
     public Mono<Customers> modify(
             @PathVariable @NotBlank @Size(min=9, max=9) String ssn,
             @Valid @RequestBody CustomerModificationRequestDto requestDto
     ) {
         return customerModificationService.modify(ssn, requestDto);
+    }
+
+    @GetMapping("{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<CustomerResponseDto> findBySsn(@PathVariable @NotBlank @Size(min=9, max=9) String ssn) {
+        return customerRetrievalService.findBySsn(ssn);
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<CustomerResponseDto> findAll() {
+        return customerRetrievalService.findAll();
     }
 
 }
---
+++ v1/src/main/java/com/api/v1/services/customers/CustomerRetrievalServiceImpl.java
@@ -0,0 +1,41 @@
+package com.api.v1.services.customers;
+
+import com.api.v1.domain.customers.CustomersRepository;
+import com.api.v1.dtos.customers.CustomerResponseDto;
+import com.api.v1.exceptions.EmptyFluxException;
+import com.api.v1.exceptions.customers.CustomerNotFoundException;
+import com.api.v1.utils.customers.CustomerFinderUtil;
+import com.api.v1.utils.customers.CustomerResponseMapper;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+class CustomerRetrievalServiceImpl implements CustomerRetrievalService {
+
+    @Autowired
+    private CustomersRepository customersRepository;
+
+    @Autowired
+    private CustomerFinderUtil customerFinderUtil;
+
+    @Override
+    public Mono<CustomerResponseDto> findBySsn(@NotBlank @Size(min=9, max=9) String ssn) {
+        return customerFinderUtil
+                .find(ssn)
+                .switchIfEmpty(Mono.error(new CustomerNotFoundException(ssn)))
+                .flatMap(customer -> Mono.just(CustomerResponseMapper.map(customer)));
+    }
+
+    @Override
+    public Flux<CustomerResponseDto> findAll() {
+        return customersRepository
+                .findAll()
+                .switchIfEmpty(Mono.error(EmptyFluxException::new))
+                .flatMap(customer -> Flux.just(CustomerResponseMapper.map(customer)));
+    }
+
+}
