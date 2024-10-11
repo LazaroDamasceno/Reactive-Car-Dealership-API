817344da8420c4bd6540b4c51eab5a5f8a04b309
lazarodamasceno <Mylittleponey1@>
2024-10-11 14:20:50 -0300

    Revert "Created the exception UnavailableCarException"
    This reverts commit 480592c8dd4dca41393eedb5ca4bd69126859bee.

--- v1/src/main/java/com/api/v1/exceptions/cars/UnavailableCarException.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.exceptions.cars;
-
-public class UnavailableCarException extends RuntimeException {
-
-    public UnavailableCarException() {
-        super("The request product is currently unavailable.");
-    }
-
-}
