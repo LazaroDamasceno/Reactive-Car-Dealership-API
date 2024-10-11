00de5d780ddfc312b36015cbb5fda16832680c80
lazarodamasceno <Mylittleponey1@>
2024-10-11 14:21:31 -0300

    Revert "Created the entity CarInventory and its repository"
    This reverts commit 80c6cab96cee68ff9fb576ac8b366491f7e528b3.

--- v1/src/main/java/com/api/v1/domain/cars/CarInventoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api.v1.domain.cars;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.util.UUID;
-
-public interface CarInventoryRepository extends ReactiveCrudRepository<CarInventory, UUID> {
-}
