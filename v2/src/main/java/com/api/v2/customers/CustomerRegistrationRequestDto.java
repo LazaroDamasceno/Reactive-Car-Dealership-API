e87d12f1ac33e40b6612d482a59474d51dcd8c6a
lazarodamasceno <Mylittleponey1@>
2024-10-13 20:21:17 -0300

    Created the dto CustomerRegistrationRequestDto

---
+++ v2/src/main/java/com/api/v2/customers/CustomerRegistrationRequestDto.java
@@ -0,0 +1,11 @@
+package com.api.v2.customers;
+
+import com.api.v2.users.dtos.UserRegistrationRequestDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+
+public record CustomerRegistrationRequestDto(
+        @NotBlank String address,
+        @Valid UserRegistrationRequestDto userRegistrationRequestDto
+) {
+}
