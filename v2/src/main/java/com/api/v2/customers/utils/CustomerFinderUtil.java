cc92e3c4f65915c7940b119c189501edd4380472
lazarodamasceno <Mylittleponey1@>
2024-10-13 21:56:39 -0300

    Created the util CustomerFinderUtil

---
+++ v2/src/main/java/com/api/v2/customers/utils/CustomerFinderUtil.java
@@ -0,0 +1,30 @@
+package com.api.v2.customers.utils;
+
+import com.api.v2.customers.CustomerNotFoundException;
+import com.api.v2.customers.domain.Customer;
+import com.api.v2.customers.domain.CustomerRepository;
+import com.api.v2.users.domain.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.experimental.UtilityClass;
+import reactor.core.publisher.Mono;
+
+@UtilityClass
+public class CustomerFinderUtil {
+
+    private UserRepository userRepository;
+    private CustomerRepository customerRepository;
+
+    public Mono<Customer> find(String ssn) {
+        return userRepository
+                .findAll()
+                .filter(e -> e.getSsn().equals(ssn))
+                .singleOrEmpty()
+                .switchIfEmpty(Mono.error(new CustomerNotFoundException(ssn)))
+                .flatMap(user -> Mono.defer(() -> customerRepository
+                            .findAll()
+                            .filter(e -> e.getUser().equals(user))
+                            .single()
+                ));
+    }
+
+}
