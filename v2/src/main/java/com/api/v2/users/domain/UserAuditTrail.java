28bf50e90d9fb43f0e9ac548cad68d7ef88a7c32
lazarodamasceno <Mylittleponey1@>
2024-10-13 21:07:52 -0300

    Renamed V2ApplicationTests as CustomerRegistrationTest

--- v2/src/main/java/com/api/v2/users/domain/UserAuditTrail.java
+++ v2/src/main/java/com/api/v2/users/domain/UserAuditTrail.java
@@ -1,6 +1,5 @@
 package com.api.v2.users.domain;
 
-import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,7 +7,6 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.util.UUID;
 
-@Getter
 @Document(collection = "user_audit_trail")
 public record UserAuditTrail(
     @Id
